53e4af799c6ed2c5dba502fbb315f71781503d4f
Times Trading <timesgacc@gmail.com>
2021-01-27 10:41:21 +0800

    hahaha h1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,7 @@
     </head>
     <body class="antialiased">
     <h1>This change is from times trading just now other add credits</h1>
+    <h1>hahahah</h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
