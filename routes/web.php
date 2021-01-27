028457448100600b2ae2a63117d3aca7754bc617
Times Trading <timesgacc@gmail.com>
2021-01-27 11:17:32 +0800

    add login controller"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+Route::get('/',[LoginController::class,'index']);
+Route::get('/welcome', function () {
     return view('welcome');
 });
