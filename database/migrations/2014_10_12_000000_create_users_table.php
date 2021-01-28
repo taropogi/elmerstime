4187ad033b1dbc355e873bad2a3b149c4c6f19c7
Times Trading <timesgacc@gmail.com>
2021-01-28 11:34:50 +0800

    Create photos migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,13 @@ class CreateUsersTable extends Migration
             $table->string('middle_name');
             $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->date('birthdate');
+            $table->string('g_first_name');
+            $table->string('g_middle_name');
+            $table->string('g_last_name');
+            $table->string('kid_relationship');
+            $table->string('contact_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
