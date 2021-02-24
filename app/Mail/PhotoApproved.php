745625f1b6adadf016bd99bec1a7e4a0c505d08d
taropogi <taropogi_123@yahoo.com>
2021-02-25 02:44:46 +0800

    Remove que

--- app/Mail/PhotoApproved.php
+++ app/Mail/PhotoApproved.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PhotoApproved extends Mailable implements ShouldQueue
+class PhotoApproved extends Mailable
 {
     use Queueable, SerializesModels;
 
